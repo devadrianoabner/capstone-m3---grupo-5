4127b64883aac8c13a2a4b24f279ee44b51836c2
Caio Giffoni <caio_cgf@hotmail.com>
2022-05-11 11:17:30 -0300

    :sparkles:initial theme created

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { ChakraProvider } from '@chakra-ui/react'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { ChakraProvider } from "@chakra-ui/react";
+import { HDRTheme } from "./theme";
+import "./theme/styles.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ChakraProvider resetCSS>
+    <ChakraProvider resetCSS theme={HDRTheme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>
 );
-
