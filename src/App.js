e5436becae99811c644208aa8036b0cd8cbdfd58
Amanda Oliveira <amandaolits@gmail.com>
2022-05-12 10:09:11 -0300

    :sparkles: signup structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import { Box, Heading, Text } from "@chakra-ui/react";
+import Routes from "./routes";
+
 
 function App() {
   return (
     <div>
-      <h1>HORA DO RANGOOOO!</h1>
+      <Routes />
     </div>
   );
 }
