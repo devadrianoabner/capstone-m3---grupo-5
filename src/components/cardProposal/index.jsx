01e0a831161c7e63bfca7ff941dcc4b5de47a04e
Caio Giffoni <caio_cgf@hotmail.com>
2022-05-18 15:07:56 -0300

    :sparkles: feature input mask price and card disappear as diet is accepted

--- src/components/cardProposal/index.jsx
+++ src/components/cardProposal/index.jsx
@@ -64,9 +64,6 @@ const CardProposal = ({ diet }) => {
             maxH={"50px"}
             w={"80%"}
             name={client.name}
-            src={
-              "https://images.unsplash.com/photo-1520810627419-35e362c5dc07?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ"
-            }
           />
 
           <Text fontSize={"20px"} fontFamily={"body"}>
